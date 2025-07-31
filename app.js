b0eddd0dcd2bfb62688cf7ef58ccd7b1f652dd70
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-31 13:29:58 +0200

    feat: add static file serving and array

--- app.js
+++ app.js
@@ -2,10 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
-
+const myArray = [{
+  title: 'Placeholder',
+  content: 'immagine',
+  img: 'img/placeholder600px.png',
+  tags: ['lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+},
+{
+  title: 'Placeholder',
+  content: 'immagine',
+  img: 'img/placeholder600px.png',
+  tags: ['lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+},
+{
+  title: 'Placeholder',
+  content: 'immagine',
+  img: 'img/placeholder600px.png',
+  tags: ['lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+},
+{
+  title: 'Placeholder',
+  content: 'immagine',
+  img: 'img/placeholder600px.png',
+  tags: ['lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+},
+{
+  title: 'Placeholder',
+  content: 'immagine',
+  img: 'img/placeholder600px.png',
+  tags: ['lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+},
+]
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
